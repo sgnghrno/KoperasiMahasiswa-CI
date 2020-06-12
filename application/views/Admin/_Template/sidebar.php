7509d84db43a4aba7abcc54a328ffa2e29eae384
sgnghrno <sgnghrno@gmail.com>
2020-06-12 18:55:13 +0700

    Create sidebar.php

---
+++ application/views/Admin/_Template/sidebar.php
@@ -0,0 +1,80 @@
+<?php
+//$getUser = $this->session->userdata('session_user');
+//$getGrup = $this->session->userdata('session_grup');
+?> 
+
+ <!-- Sidebar -->
+ <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+<!-- Sidebar - Brand -->
+<a class="sidebar-brand d-flex align-items-center justify-content-center" href="<?php echo base_url().'saldo/home'; ?>">
+  <div class="sidebar-brand-icon rotate-n-15">
+    <i class="fas fa-laugh-wink"></i>
+  </div>
+  <div class="sidebar-brand-text mx-3">Koperasi<sup>V1</sup></div>
+</a>
+
+<!-- Divider -->
+<hr class="sidebar-divider my-0">
+
+<!-- Nav Item - Home -->
+<li class="nav-item active">
+  <a class="nav-link" href="<?php echo base_url().'saldo/home'; ?>">
+    <i class="fas fa-fw fa-tachometer-alt"></i>
+    <span>Home</span></a>
+</li>
+
+<!-- Divider -->
+<hr class="sidebar-divider">
+
+<!-- Heading -->
+<div class="sidebar-heading">
+  Laporan
+</div>
+
+<!-- Nav Item - Charts -->
+<li class="nav-item">
+  <a class="nav-link" href="<?php echo base_url().'saldo'; ?>">
+    <i class="fas fa-fw fa-dollar-sign"></i>
+    <span>Keuangan</span></a>
+</li>
+
+<!-- Nav Item - Tables -->
+<li class="nav-item">
+  <a class="nav-link" href="<?php echo base_url().'home'; ?>">
+    <i class="fas fa-fw fa-clipboard-list"></i>
+    <span>Kepengurusan</span></a>
+</li>
+
+<!-- Divider -->
+<hr class="sidebar-divider">
+
+<!-- Heading -->
+<div class="sidebar-heading">
+  Laporan
+</div>
+
+<!-- Nav Item - Charts -->
+<li class="nav-item">
+  <a class="nav-link" href="<?php echo base_url().'saldo/d_saldo'; ?>">
+    <i class="fas fa-fw fa-table"></i>
+    <span>Data Simpanan</span></a>
+</li>
+
+<!-- Nav Item - Tables -->
+<li class="nav-item">
+  <a class="nav-link" href="<?php echo base_url().'home/d_anggota'; ?>">
+    <i class="fas fa-fw fa-calendar"></i>
+    <span>Data Kepengurusan</span></a>
+</li>
+
+<!-- Divider -->
+<hr class="sidebar-divider d-none d-md-block">
+
+<!-- Sidebar Toggler (Sidebar) -->
+<div class="text-center d-none d-md-inline">
+  <button class="rounded-circle border-0" id="sidebarToggle"></button>
+</div>
+
+</ul>
+<!-- End of Sidebar -->
